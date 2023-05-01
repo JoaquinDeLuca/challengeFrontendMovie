1681e5c600cca945df4cdddfd40e3349d34b7dcd
JoaquinDeLuca <joaqui222@gmail.com>
2023-05-01 20:45:10 -0300

    se agregan className para darle estilos

--- src/components/DetailsMovie/DetailsCard.tsx
+++ src/components/DetailsMovie/DetailsCard.tsx
@@ -13,32 +13,31 @@ export default function DetailsCard({data}:Props) {
 
   return (
     <>
-    <div className="containerDetails">
-      <img className="imgDetails" src={`https://image.tmdb.org/t/p/w500${poster_path}`} alt={title} />
-      <div className="info">
-        <Typography variant="h3">{title}</Typography>
-        <Typography>{overview}</Typography>
-        <Typography><Typography component={'span'} fontWeight={'500'}>Votes:</Typography> {vote_average}</Typography>
-        <Typography><Typography component={'span'} fontWeight={'500'}>Realease date: </Typography>{release_date.toString()}</Typography>
-        <Typography><Typography component={'span'} fontWeight={'500'}>Orginal Language: </Typography>{original_language}</Typography>
-        {/* Si exite cada una de esas propiedades voy a mostrar el comentario */}
-        {comment && nameUser && 
-        <>
-          <Typography variant="h5" marginTop={2}>Comments</Typography>
-          <Typography fontWeight={'600'}>{nameUser}</Typography>
-          <Typography>{comment}</Typography>
-        </>
-        }
-        {/* logica para poder añadir un comentario  */}
-        <Box>
-          { <Button variant="outlined" onClick={() => setShowInputs(!Showinputs)} sx={{marginTop: 4}}>Add Comment</Button>}
-        </Box>
-        <Box sx={{display: 'flex',flexDirection: 'column' , width: '360px',}}>
-          {Showinputs && <Comments id={id} key={id} />}
-        </Box>
+      <div className="containerDetails">
+        <img className="imgDetails" src={`https://image.tmdb.org/t/p/w500${poster_path}`} alt={title} />
+        <div className="info">
+          <Typography variant="h3">{title}</Typography>
+          <Typography>{overview}</Typography>
+          <Typography><Typography component={'span'} fontWeight={'500'}>Votes:</Typography> {vote_average}</Typography>
+          <Typography><Typography component={'span'} fontWeight={'500'}>Realease date: </Typography>{release_date.toString()}</Typography>
+          <Typography><Typography component={'span'} fontWeight={'500'}>Orginal Language: </Typography>{original_language}</Typography>
+          {/* Si exite cada una de esas propiedades voy a mostrar el comentario */}
+          {comment && nameUser && 
+          <>
+            <Typography variant="h5" marginTop={2}>Comments</Typography>
+            <Typography fontWeight={'600'}>{nameUser}</Typography>
+            <Typography>{comment}</Typography>
+          </>
+          }
+          {/* logica para poder añadir un comentario  */}
+          <Box>
+            { <Button variant="outlined" onClick={() => setShowInputs(!Showinputs)} sx={{marginTop: 4}}>Add Comment</Button>}
+          </Box>
+          <Box width={'255px'}>
+            {Showinputs && <Comments id={id} key={id} />}
+          </Box>
+        </div>
       </div>
-    </div>
-        
     </>
   );
 }
